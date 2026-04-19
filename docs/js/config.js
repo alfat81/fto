675589155e3900a28dbf9ca0dc181ef9cec3063e
alfat81 <52703823+alfat81@users.noreply.github.com>
2026-04-19 19:26:13 +0300

    1.4

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,7 +2,8 @@
     app: { name: 'ФТО', version: '3.1', currency: '₽' },
     contacts: { phone: '+7 (960) 178-67-38', email: 'a20072005@yandex.ru', address: 'г. Нижний Новгород, ул. Тургенева, 9' },
     telegram: {
-    token: '',  // ПУСТОЙ ТОКЕН = отключено
-    chatId: ''
+        token: '',  // ПУСТОЙ ТОКЕН = отключено
+        chatId: ''
+    },
     paths: { imagesBase: 'images/' }
 };
